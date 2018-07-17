b66eeb546972563a8e01c946dc98e8a7bc602281
Noah <noah@gangplankhq.com>
2018-07-17 09:57:16 -0700

    displayed articles to cards

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -9,15 +9,7 @@ var route;
 var headline;
 var snippet;
 var nytLink;
-var headline1;
-var snippet1;
-var nytLink1;
-var headline2;
-var snippet2;
-var nytLink2;
-var headline3;
-var snippet3;
-var nytLink3;
+
 
 function drugSearch(search) {
     var fdaurl = "https://api.fda.gov/drug/label.json?search=openfda.brand_name:(" + search + ")&limit=1&skip=0?key=" + key;
@@ -44,7 +36,7 @@ function drugSearch(search) {
 
     var nyturl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     nyturl += '?' + $.param({
-        'api-key': "29682e6e1b2e47189cfaa9a29501b31a",  
+        'api-key': "29682e6e1b2e47189cfaa9a29501b31a",
         'fq': "headline.search:" + search,
         'sort': "newest"
     });
@@ -53,7 +45,8 @@ function drugSearch(search) {
         method: 'GET',
     }).then(function (e) {
         console.log(e);
-            for(var i = 0; i < e.response.docs.length; i++){
+
+        for (var i = 0; i < e.response.docs.length; i++) {
             var headline = e.response.docs[i].headline.main;
             var snippet = e.response.docs[i].snippet;
             var nytLink = e.response.docs[i].web_url;
@@ -61,16 +54,16 @@ function drugSearch(search) {
             console.log(snippet)
             console.log(nytLink)
         }
+
     })
 }
 
+// $("form").on("submit", function (e) {
+//     e.preventDefault();
 
-$("form").on("submit", function (e) {
-    e.preventDefault();
-
-    var drug = $("#userSearch").val().trim()
-    drugSearch(drug);
-})
+//     var drug = $("#userSearch").val().trim()
+//     drugSearch(drug);
+// })
 
 var homeurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 homeurl += '?' + $.param({
@@ -85,22 +78,12 @@ $.ajax({
     console.log(e);
 
     for (var i = 0; i < 3; i++) {
-        var headline1 = e.response.docs[0].headline.main;
-        var snippet1 = e.response.docs[0].snippet;
-        var nytLink1 = e.response.docs[0].web_url;
-        $("#header-1").html(headline1);
-        $("#para-1").html(snippet1)
-        var headline2 = e.response.docs[1].headline.main;
-        var snippet2 = e.response.docs[1].snippet;
-        var nytLink2 = e.response.docs[1].web_url;
-        $("#header-2").html(headline2);
-        $("#para-2").html(snippet2)
-        var headline3 = e.response.docs[2].headline.main;
-        var snippet3 = e.response.docs[2].snippet;
-        var nytLink3 = e.response.docs[2].web_url;
-        $("#header-3").html(headline3);
-        $("#para-3").html(snippet3)
-
+        var headline = e.response.docs[i].headline.main;
+        var snippet = e.response.docs[i].snippet;
+        var nytLink = e.response.docs[i].web_url;
+        console.log(headline)
+        console.log(snippet)
+        console.log(nytLink)
     }
 
 })
