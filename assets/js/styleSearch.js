e2e0b38f07b518ddf4efac90326ea9933475d40c
Avonlea Haymart <avhaymart@gmail.com>
2018-07-18 10:48:42 -0700

    fixed bugs

--- assets/js/styleSearch.js
+++ assets/js/styleSearch.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
-    $(".accordion").accordion();
-    $("#navBrand").on("click", function(){
-        window.location.href("./index.html");
-    })
+    $(".accordion").accordion({
+        collapsible: true,
+        active: 'none',
+        autoHeight: false,
+        navigation: true
+    });
 });
