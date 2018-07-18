71fd18e53070e54f44a3da34044a8cae6101c410
Avonlea Haymart <avhaymart@gmail.com>
2018-07-18 10:21:10 -0700

    Fixed bugs

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("#nav-container").fadeIn(300, function () {
-        $("#hero-container").fadeIn(300, function(){
+        $("#hero-container").fadeIn(300, function () {
             $("#articles-container").fadeIn(300);
         })
         $("#hero-container").css("display", "flex");
--- assets/js/styleSearch.js
+++ assets/js/styleSearch.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
     $(".accordion").accordion();
+    $("#navBrand").on("click", function(){
+        window.location.href("./index.html");
+    })
 });
